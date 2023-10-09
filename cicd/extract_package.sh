b779c5790add0f89981765d60284ea9a5f021a11
Benjamin Labbé <benjamin.labbe@cea.fr>
2023-10-09 17:19:36 +0200

    update script to extract package from docker image

--- cicd/extract_package.sh
+++ cicd/extract_package.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+set -u
+set -e
+
+if [ "$#" -ne 3 ]; then
+ echo "Expect 3 arguments : <dockerImage> <packageDir> <outputEnvFile>"
+ exit 1;
+fi
 
 dockerImage=$1
 packageDir=$2
+GITHUB_OUTPUT=$3
+
 id=$(docker run -d "$dockerImage" sleep infinity)
 echo -n "export fullname=$packageDir/" > ./envFile
 docker exec $id ls $packageDir >> ./envFile
@@ -11,6 +20,14 @@ echo "tmp=\${trimmed#*-}" >> ./envFile
 echo "export version=\${tmp%-*}" >> ./envFile
 echo "filename=$(. envFile && echo $filename)" >> $GITHUB_OUTPUT
 echo "version=$(. envFile && echo $version)"  >> $GITHUB_OUTPUT
-. envFile && docker cp $id:$fullname - > ./$filename
+. ./envFile && echo "filename=$filename , version=$version"
+echo "Extract file from docker image"
+. ./envFile && docker cp $id:$fullname - > ./$filename
 docker stop $id
 docker rm -v $id
+echo "Finaly, the package should be present in local folder : "
+if [ -f "./$filename" ]; then echo "!! yes : ./$filename" ; else echo "!! no"; exit 1; fi
+
+
+
+
